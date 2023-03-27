a283957e414dd33df9503b2f1d9f11aac203f8f2
Coursera <aide.abdel@gmail.com>
2023-03-27 01:44:55 -0400

    mealplanner

--- src/pages/MealPlanner.js
+++ src/pages/MealPlanner.js
@@ -2,10 +2,39 @@ import React, { useState } from "react";
 import Menu from "../components/Menu";
 
 function MealPlanner() {
+  const daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  const mealsOfDay = ["Breafast", "Lunch", "Snack", "Dinner"];
+
+  const renderDay = (day) => {
+    return (
+      <div className="day">
+        <h3>{day}</h3>
+        {mealsOfDay.map((hour) => (
+          <div className="hour">
+            <span className="time">{hour}</span>
+          </div>
+        ))}
+      </div>
+    );
+  };
+
   return (
-    <div>
+    <>
       <Menu />
-    </div>
+      <div className="planner">
+        <h2>Meal Planner</h2>
+        <div className="week">{daysOfWeek.map((day) => renderDay(day))}</div>
+      </div>
+    </>
   );
 }
 
